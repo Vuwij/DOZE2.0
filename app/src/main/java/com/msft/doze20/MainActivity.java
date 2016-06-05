44f7eecfb78cb0742070aa6380f572fc9ad0a51e
Victoria Cheng <victoria.j.cheng@gmail.com>
2016-06-04 22:48:47 -0400

    Added battery life indicator

--- app/src/main/java/com/msft/doze20/MainActivity.java
+++ app/src/main/java/com/msft/doze20/MainActivity.java
@@ -1,11 +1,15 @@
 package com.msft.doze20;
 
+import android.app.DatePickerDialog;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,9 +27,10 @@ public class MainActivity extends AppCompatActivity {
         registerReceiver(screenReceiver, filter);
     }
 
+
     @Override
-    protected void onPause(){
-        if (ScreenReceiver.wasScreenOn){
+    protected void onPause() {
+        if (ScreenReceiver.wasScreenOn) {
             System.out.println("SCREEN TURNED OFF Activity");
         }
 
@@ -33,10 +38,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
 
         if (!ScreenReceiver.wasScreenOn)
             System.out.println("SCREEN TURNED ON Activity");
         super.onResume();
     }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+        String time = "You picked the following time: "+hourOfDay+"h"+minute;
+        timeTextView.setText(time);
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        String date = "You picked the following date: "+dayOfMonth+"/"+(monthOfYear+1)+"/"+year;
+        dateTextView.setText(date);
+    }
 }
+this.registerReceiver(this.batteryInfoReceiver,new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+private BroadcastReceiver batteryInfoReceiver=new BroadcastReceiver(){
+@Override
+public void onReceive(Context context,Intent intent){
+        // this is where we deal with the data sent from the battery.
+        }
+        };

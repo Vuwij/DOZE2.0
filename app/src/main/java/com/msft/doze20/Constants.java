4b711712828c6aec8539ba52f7f59f1f45709b54
Victoria Cheng <victoria.j.cheng@gmail.com>
2016-06-04 22:46:22 -0400

    Added battery indicator reciever

--- app/src/main/java/com/msft/doze20/Constants.java
+++ app/src/main/java/com/msft/doze20/Constants.java
@@ -7,10 +7,14 @@ public class Constants {
 
     public static final int TYPE_SCREEN = 0;
     public static final int TYPE_CHARGING = 1;
+    public static final int TYPE_BATTERY = 2;
 
     public static final int DATA_SCREEN_OFF = 0;
     public static final int DATA_SCREEN_ON = 1;
     public static final int DATA_CHARGING_OFF = 0;
     public static final int DATA_CHARGING_ON = 1;
+    public static final int DATA_BATTERY_LOW = 0;
+    public static final int DATA_BATTERY_OKAY = 1;
+
 
 }
--- app/src/main/java/com/msft/doze20/MainActivity.java
+++ app/src/main/java/com/msft/doze20/MainActivity.java
@@ -10,12 +10,14 @@ import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     BroadcastReceiver screenReceiver;
+    BroadcastReceiver batteryReceiver;
     private PendingIntent pendingIntent;
     private AlarmManager manager;
 
@@ -30,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
         filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
         screenReceiver = new ScreenReceiver();
         registerReceiver(screenReceiver, filter);
+
+        IntentFilter filter2 = new IntentFilter(Intent.ACTION_BATTERY_LOW);
+        filter2.addAction(Intent.ACTION_BATTERY_OKAY);
+        batteryReceiver = new BatteryReceiver();
+        registerReceiver(batteryReceiver, filter2);
 
         Intent alarmIntent = new Intent(this, updateToServer.class);
         pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
@@ -65,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         System.out.print("Destroyed");
         unregisterReceiver(screenReceiver);
+        unregisterReceiver(batteryReceiver);
         cancelAlarm();
     }
 }
